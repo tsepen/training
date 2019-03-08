c6968150029bf78cbcea975e61a749ecb75c3ca3
Andrey Okhotnikov <an.okhotnikov@gmail.com>
2019-03-08 09:13:41 +0300

    add bootstrap

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import { Provider } from 'react-redux';
 
 import Router from './router'
 
+import 'bootstrap/dist/css/bootstrap.css';
+
 import * as serviceWorker from './serviceWorker';
 
 import configureStore from './store';
@@ -19,4 +21,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.register();
+serviceWorker.unregister();
